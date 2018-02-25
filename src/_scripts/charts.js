6b82728bb876b5b3256065382e5f17239ed117e8
palewire <ben.welsh@gmail.com>
2018-02-25 08:41:17 -0800

    Ugly table

--- src/_scripts/charts.js
+++ src/_scripts/charts.js
@@ -3,9 +3,15 @@ var Plotlybar = require('plotly.js/lib/bar');
 
 Plotly.register(Plotlybar);
 
-var countyHomicides = annualTotals.map(a => a.homicides_total);
-var harvardParkHomicides = annualTotals.map(a => a.homicides_harvard_park);
-var years = annualTotals.map(a => a.year);
+var countyHomicides = [];
+var harvardParkHomicides = [];
+var years = [];
+
+annualTotals.forEach(function(row) {
+  countyHomicides.push(row['homicides_total']);
+  harvardParkHomicides.push(row['homicides_harvard_park']);
+  years.push(row['year']);
+});
 
 function createChart(x, y, element, title) {
   var settings = [{
