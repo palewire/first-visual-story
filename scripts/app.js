da596382542386a9a15b73253546a5b4bba25669
palewire <b@palewi.re>
2022-02-25 16:20:46 -0800

    Cut

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,2 @@
 // Write your code!
-console.log("Hello bakers");
-
-// Grab some data
-const dataElement = document.getElementById('example-data');
-const data = JSON.parse(dataElement.textContent);
-console.log(data);
+console.log('Hello bakers');
