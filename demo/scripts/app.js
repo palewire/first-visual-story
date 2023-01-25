d0ae31213e54c7ef0f9bf9a268f7ecbc04b5856e
palewire <b@palewi.re>
2023-01-25 09:46:21 -0800

    Simplify the JS code in the demo

--- demo/scripts/app.js
+++ demo/scripts/app.js
@@ -1,3 +1,31 @@
-// Write your code!
-console.log('Hello bakers');
-import './map';
+// Import dependencies
+import * as L from 'leaflet';
+
+// Import data
+import homicides from '../_data/harvard_park_homicides.json';
+
+// Set the id of the div on the page where the map will go
+const divId = 'map';
+
+// Create the map
+const map = L.map(divId, {
+  scrollWheelZoom: false,
+});
+
+// Add a satellite layer
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA',
+  {
+    minZoom: 13,
+  }
+).addTo(map);
+
+// Set the center and zoom
+map.setView([33.983265, -118.306799], 18);
+
+// Load the data
+homicides.forEach((obj) => {
+  L.circleMarker([obj.latitude, obj.longitude])  // As a pin ...
+    .addTo(map)
+    .bindTooltip(obj.first_name + ' ' + obj.last_name, { permanent: true });  // ... with a tooltip
+});
